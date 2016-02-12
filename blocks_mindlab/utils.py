021e6a3fe5ca46cbdb8035ecfd982820eb7bc10a
John Arevalo <jearevaloo@unal.edu.co>
2016-02-12 12:09:54 -0500

    Add OneHotTransformer

--- blocks_mindlab/utils.py
+++ blocks_mindlab/utils.py
@@ -1,8 +1,10 @@
+import numpy
 import os.path
 import yaml
 from theano import tensor
 from bokeh.plotting import output_server, cursession
 from bokeh.document import Document
+from fuel.transformers import AgnosticSourcewiseTransformer
 
 
 def get_measures(y_true, y_pred):
@@ -21,3 +23,17 @@ def get_measures(y_true, y_pred):
     fn.name = 'fn'
     f_score.name = 'f_score'
     return tp, tn, fp, fn, pre, rec, f_score
+
+
+class OneHotTransformer(AgnosticSourcewiseTransformer):
+
+    def __init__(self, data_stream, nclasses, **kwargs):
+        self.nclasses = nclasses
+        self.I = numpy.eye(self.nclasses, dtype='int32')
+        if data_stream.axis_labels:
+            kwargs.setdefault('axis_labels', data_stream.axis_labels.copy())
+        super(OneHotTransformer, self).__init__(
+            data_stream, data_stream.produces_examples, **kwargs)
+
+    def transform_any_source(self, source_data, _):
+        return self.I[source_data]
